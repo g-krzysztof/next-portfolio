abe9d1a2d76aaf7904a7cb48c8a8b0c2f1c2e677
g-krzysztof <hello@krzysztofg.pl>
2023-02-19 13:07:27 +0100

    add ContentContainer component

---
+++ components/Menu/Menu.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+import { Box } from '../../styles';
+
+const Menu = () => {
+
+  return (
+    <MenuWrapper>
+      Menu
+    </MenuWrapper>
+  );
+};
+
+export default Menu;
+
+const MenuWrapper = styled(Box)`
+
+`;
